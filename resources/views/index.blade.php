855d5b8d70edbae0028bbd6dea536c3cc0ebe649
Gkiokan Sali <gkiokan@hotmail.de>
2019-04-03 02:43:45 +0200

    clean up gitignore files in storage folder and add the favicon to the html markup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,7 +25,7 @@ $polyfills = [
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <title>{{ config('app.name') }}</title>
-
+  <link rel="shortcut icon" type="image/x-icon" href="/limehome-favicon.png">
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
