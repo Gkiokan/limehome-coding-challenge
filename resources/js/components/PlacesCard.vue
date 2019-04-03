2f15477f5ac315b44d27942be7b0276f64aacb4c
Gkiokan Sali <gkiokan@hotmail.de>
2019-04-03 15:50:56 +0200

    remove cursor style from card.

--- resources/js/components/PlacesCard.vue
+++ resources/js/components/PlacesCard.vue
@@ -76,7 +76,6 @@ export default {
       border-radius: 5px;
       box-shadow: 0 1px 2px rgba(0, 0, 0, 0.25);
       overflow: hidden;
-      cursor: pointer;
 
       .image {
           position: relative;
