8ead89111d9f79b2a5f88ca0c8bfec6b74c92ea6
Gkiokan Sali <gkiokan@hotmail.de>
2019-04-03 11:00:32 +0200

    Define fillable fields in Bookings Model

--- app/Bookings.php
+++ app/Bookings.php
@@ -7,8 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Bookings extends Model
 {
 
+    protected $fillable = [
+        'user_id',
+        'name', 'adress', 'city', 'phone',
+        'place_id', 'plus_code',
+    ]
+
     public function user(){
         return $this->belongsTo(App\User::class);
     }
-    
+
 }
